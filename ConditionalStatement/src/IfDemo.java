36d6aa799d09cdc0363cfb571ba44d461fff0462
Morla Manish <manishmorla03@gmail.com>
2025-08-16 15:24:54 +0530

    initial commit

---
+++ ConditionalStatement/src/IfDemo.java
@@ -0,0 +1,14 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 6;
+		if (x != 5)
+		System.out.println("Value of x is not 5");
+		System.out.println("Value of x is has to be 5");
+		 
+
+	}
+
+}
---
+++ ConditionalStatement/src/IfElse.java
@@ -0,0 +1,18 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int age=18;
+        
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else
+			System.out.println("Sorry! You need to wait more");
+
+		System.out.println("if else demonstration");
+
+	}
+
+}
